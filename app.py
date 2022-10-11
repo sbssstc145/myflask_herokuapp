14d40fd58b1c9e121136fc202d2dc03b666f96ab
sbssstc145 <114373390+sbssstc145@users.noreply.github.com>
2022-10-11 16:03:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__) #creating the Flask class object
  
 @app.route('/') #decorator drfines the   
 def home():  
-    return "hello, this is our first flask website";  
+    return "hello, this is our first flask website" 
   
 if __name__ =='__main__':  
     app.run(debug = True)  
